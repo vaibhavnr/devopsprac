f2ef23c9efa3b1cdebb2ea1e4a7ad5017d940451
vaibhavnr <nrvaibhavtej@gmail.com>
2023-08-20 18:43:30 +0530

    Removed extra printing lines

--- Addtion.java
+++ Addtion.java
@@ -9,11 +9,12 @@ n1 = sc.nextInt();
 System.out.print("Enter n2: "); 
 n2 = sc.nextInt();  
 res = n1 + n2;    
-System.out.println("The sum of numbers is: "+sum); 
-System.out.println("Sample line 1");
-System.out.println("Sample line 2 ");
-System.out.println("Sample line 3 ");
-System.out.println("Sample line 4");
-System.out.println("Sample line 5 ");
-}  
-} 
+System.out.println("The sum of numbers is: "+sum);
+
+
+
+
+}
+}
+
+

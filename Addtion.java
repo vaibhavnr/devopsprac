060c7f77c37127efd28ecbe1e8472b18421c88b1
vaibhavnr <141004157+vaibhavnr@users.noreply.github.com>
2023-08-20 18:27:46 +0530

    Create Addtion.java

---
+++ Addtion.java
@@ -0,0 +1,19 @@
+public class addition
+{  
+public static void main(String args[])   
+{  
+int n1, n2, res;  
+Scanner sc = new Scanner(System.in);  
+System.out.print("Enter n1: ");  
+n1 = sc.nextInt();  
+System.out.print("Enter n2: "); 
+n2 = sc.nextInt();  
+res = n1 + n2;    
+System.out.println("The sum of numbers is: "+sum); 
+System.out.println("Sample line 1");
+System.out.println("Sample line 2 ");
+System.out.println("Sample line 3 ");
+System.out.println("Sample line 4");
+System.out.println("Sample line 5 ");
+}  
+} 

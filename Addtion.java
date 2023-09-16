f6736744685eb67e06eb095e315d08bd6aafb2a1
vaibhavnr <nrvaibhavtej@gmail.com>
2023-09-16 21:06:00 +0530

    multiplicxation added

--- Addtion.java
+++ Addtion.java
@@ -10,8 +10,11 @@ System.out.print("Enter n2: ");
 n2 = sc.nextInt();  
 res = n1 + n2;    
 System.out.println("The sum of numbers is: "+res);
-res = n1 + n2;    
+res = n1 - n2;    
 System.out.println("The Sub of numbers is: "+res);
+res = n1 * n2;
+System.out.println("The Sub of numbers is: "+res);
+
 }
 }
 
